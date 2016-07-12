b79233428b060e4e562e915529b0fab0b7b5b76c
Alex Mathews <alexjmathews@gmail.com>
2016-07-12 18:12:30 -0400

    with options support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*
-*   File:       index.js (pdffiller)
-*   Project:    PDF Filler
-*   Date:       May 2015.
-*
-*   Description: This PDF filler module takes a data set and creates a filled out
-*                PDF file with the form fields populated.
-*/
-(function(){
+ *   File:       index.js (pdffiller)
+ *   Project:    PDF Filler
+ *   Date:       May 2015.
+ *
+ *   Description: This PDF filler module takes a data set and creates a filled out
+ *                PDF file with the form fields populated.
+ */
+(function() {
     var child_process = require('child_process'),
         exec = require('child_process').exec,
         _ = require('lodash'),
@@ -14,12 +14,12 @@
 
     var pdffiller = {
 
-        mapForm2PDF: function( formFields, convMap ){
+        mapForm2PDF: function(formFields, convMap) {
             var tmpFDFData = this.convFieldJson2FDF(formFields);
-            tmpFDFData = _.mapKeys(tmpFDFData, function(value, key){
+            tmpFDFData = _.mapKeys(tmpFDFData, function(value, key) {
                 try {
                     convMap[key];
-                } catch(err){
+                } catch (err) {
 
                     return key;
                 }
@@ -29,14 +29,14 @@
             return tmpFDFData;
         },
 
-        convFieldJson2FDF: function(fieldJson){
+        convFieldJson2FDF: function(fieldJson) {
             var _keys = _.pluck(fieldJson, 'title'),
                 _values = _.pluck(fieldJson, 'fieldValue');
 
-            _values = _.map(_values, function(val){
-                if(val === true){
+            _values = _.map(_values, function(val) {
+                if (val === true) {
                     return 'Yes';
-                }else if(val === false) {
+                } else if (val === false) {
                     return 'Off';
                 }
                 return val;
@@ -47,37 +47,50 @@
             return jsonObj;
         },
 
-        generateFieldJson: function( sourceFile, nameRegex, callback){
+        generateFieldJson: function(sourceFile, nameRegex, callback) {
             var regName = /FieldName: ([^\n]*)/,
+                regOptionsAll = /FieldStateOption: ([A-Za-z\t .]+)/g,
+                regOptions = /FieldStateOption: ([A-Za-z\t .]+)/,
                 regType = /FieldType: ([A-Za-z\t .]+)/,
                 regFlags = /FieldFlags: ([0-9\t .]+)/,
                 fieldArray = [],
                 currField = {};
 
-            if(nameRegex !== null && (typeof nameRegex) == 'object' ) regName = nameRegex;
+            if (nameRegex !== null && (typeof nameRegex) == 'object') regName = nameRegex;
 
-            exec( "pdftk " + sourceFile + " dump_data_fields_utf8 " , function (error, stdout, stderr) {
+            exec("pdftk " + sourceFile + " dump_data_fields_utf8 ", function(error, stdout, stderr) {
                 if (error) {
                     console.log('exec error: ' + error);
                     return callback(error, null);
                 }
 
                 fields = stdout.toString().split("---").slice(1);
-                fields.forEach(function(field){
+                fields.forEach(function(field) {
+
                     currField = {};
 
                     currField['title'] = field.match(regName)[1].trim() || '';
 
-                    if(field.match(regType)){
+                    if (field.match(regType)) {
                         currField['fieldType'] = field.match(regType)[1].trim() || '';
-                    }else {
+                    } else {
                         currField['fieldType'] = '';
                     }
 
-                    if(field.match(regFlags)){
-                        currField['fieldFlags'] = field.match(regFlags)[1].trim()|| '';
-                    }else{
+                    if (field.match(regFlags)) {
+                        currField['fieldFlags'] = field.match(regFlags)[1].trim() || '';
+                    } else {
                         currField['fieldFlags'] = '';
+                    }
+
+                    if (field.match(regOptionsAll)) {
+                        var all_opts = field.match(regOptionsAll);
+                        var options = [];
+                        all_opts.forEach(function(value){
+                            options.push(value.match(regOptions)[1].trim() || '');
+                        });
+                        currField['fieldOptions'] = options;
+                    } else {
                     }
 
                     currField['fieldValue'] = '';
@@ -89,14 +102,14 @@
             });
         },
 
-        generateFDFTemplate: function( sourceFile, nameRegex, callback ){
-            this.generateFieldJson(sourceFile, nameRegex, function(err, _form_fields){
+        generateFDFTemplate: function(sourceFile, nameRegex, callback) {
+            this.generateFieldJson(sourceFile, nameRegex, function(err, _form_fields) {
                 if (err) {
-                  console.log('exec error: ' + err);
-                  return callback(err, null);
+                    console.log('exec error: ' + err);
+                    return callback(err, null);
                 }
-                var _keys   = _.pluck(_form_fields, 'title'),
-                  _values = _.pluck(_form_fields, 'fieldValue'),
+                var _keys = _.pluck(_form_fields, 'title'),
+                    _values = _.pluck(_form_fields, 'fieldValue'),
                     jsonObj = _.zipObject(_keys, _values);
 
                 return callback(null, jsonObj);
